f085595c03cd513e2112eb1852d69c6845e26b19
Randy Dev <killerx@randydev.my.id>
2025-01-22 17:37:03 +0700

    add copilot

---
+++ plugins/copilot.js
@@ -0,0 +1,48 @@
+/*
+Credits @xpushz on telegram 
+Copyright 2017-2025 (c) Randy W @xtdevs, @xtsea on telegram
+from : https://github.com/TeamKillerX
+Channel : @RendyProjects
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+import express from 'express';
+import { Copilot2Trip } from '../lib/scrapper.js';
+const CopilotRoutes = express.Router();
+
+/**
+ * @swagger
+ * /api/v1/ai/copilot2-trip:
+ *   get:
+ *     summary: copilot2 Trip
+ *     tags: [AI]
+ *     parameters:
+ *       - in: query
+ *         name: query
+ *         required: true
+ *         description: The query to be processed.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+CopilotRoutes.get("/api/v1/ai/copilot2-trip", async (req, res) => {
+    try {
+        const query = req.query.query;
+        const results = await Copilot2Trip(query);
+        res.json({ results });
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+});
+
+export { CopilotRoutes };
