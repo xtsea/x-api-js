44a95b5fdcfb88451e4b5e514f73520d82fdd869
Randy Dev <killerx@randydev.my.id>
2025-01-23 11:21:19 +0700

    Update config.js
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- config.js
+++ config.js
@@ -7,11 +7,10 @@ const GoogleAPIKey = process.env.GOOGLE_API_KEY;
 const BotToken = process.env.BOT_TOKEN;
 const IsTelegramBot = false;
 
-if (IsTelegramBot) {
-  if(!BotToken) {
-    throw new Error("Required `BOT_TOKEN` ");
-  }
+if (IsTelegramBot && !BotToken) {
+      throw new Error("Required `BOT_TOKEN` ");
 }
+
 
 if (!dbUri && !GoogleAPIKey) {
     throw new Error("Missing variables required");
